5b3c1a40879052656b8ad85379d73b0232c63fe1
obi3112 <98112470+marioboi3112@users.noreply.github.com>
2024-03-09 11:55:16 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,14 @@
+-- title:   game title
+-- author:  game developer, email, etc.
+-- desc:    game for the minigame a month game jam
+-- site:    website link
+-- license: MIT License (change this to your license of choice)
+-- version: 0.1
+-- script:  lua
+
+t=0 --time variable
+
+function TIC()
+	cls(0)
+	print("HELLO WORLD", 0, 0, 3)
+end
