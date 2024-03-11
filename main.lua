af0213c8886d2e77fb1497d8c0930e41ed1b5a45
obi3112 <98112470+marioboi3112@users.noreply.github.com>
2024-03-11 16:31:52 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,10 +5,22 @@
 -- license: MIT License (change this to your license of choice)
 -- version: 0.1
 -- script:  lua
-
-t=0 --time variable
+local t = 0 --time variable
+function title_screen()
+	local titleCol = {
+		["15"] = 15,
+		["14"] = 14,
+		["13"] = 13,
+	}
+	local title = "draze studios"
+	print(title, 240/2, 136/2,titleCol["15"])
+	if (t % 1000 >= 2) then
+		titleCol["15"] = titleCol["15"] - 1
+	end
+end
 
 function TIC()
-	cls(0)
-	print("HELLO WORLD", 0, 0, 3)
+	cls(1)
+	title_screen()
+ t=t+1
 end
