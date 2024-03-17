3551b53b196b9c56dc63cb2b8a65d1bc914c35f6
obi3112 <drazeobi3112@gmail.com>
2024-03-16 20:07:06 -0500

    added buttons and shit

--- main.lua
+++ main.lua
@@ -5,19 +5,50 @@
 -- license: MIT License (change this to your license of choice)
 -- version: 0.1
 -- script:  lua
-local t = 0 --time variable
-function title_screen()
-	local titleCol = {
-		["15"] = 15,
-		["14"] = 14,
-		["13"] = 13,
+
+-- A FEW CUSTOM PLUGINS
+function Button(x,y,w,h,col)
+	return {
+		x = x,
+		y = y,
+		w = w,
+		h = h,
+		col = col,
+		createButton = function(self)
+			rect(self.x, self.y, self.w, self.h, self.col)
+		end,
+		clickButton = function(self)
+			if (mouse()
+		end,
 	}
+end
+
+
+local t = 0 --time variable
+
+function title_screen()
+	
 	local title = "draze studios"
-	print(title, 240/2, 136/2,titleCol["15"])
-	if (t % 1000 >= 2) then
-		titleCol["15"] = titleCol["15"] - 1
+	print(title, 240/2, 136/2,15)
+	if (t >= 200) then
+		cls(2)
+		menu()
 	end
 end
+
+function menu()
+	local title_data = { --table consisting of data for title
+		str = "DUNGEONEER",
+		
+	}
+	local bttn = Button(10, 10, 240-20, 20, 4)
+	bttn:createButton()
+	print(title_data.str, 10+3,10+2,2,false, 2, false)
+	
+	--buttons for clicking events
+end
+
+
 
 function TIC()
 	cls(1)
